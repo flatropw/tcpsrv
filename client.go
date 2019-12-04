f9441de50bd6f29e09b65bddfe7c8ba7dabd5079
root <changelater@mail.com>
2019-12-04 23:00:26 +0300

    add simple tcp client

--- client.go
+++ client.go
@@ -1,1 +1,34 @@
-package tcpsrv
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+)
+
+func main() {
+	conn, _ := net.Dial("tcp", address+":"+port)
+	for {
+		reader := bufio.NewReader(os.Stdin)
+
+		fmt.Print("Request:")
+
+		text, err := reader.ReadString(delimiter)
+		if err != nil {
+			panic(err)
+		}
+
+		_, err = fmt.Fprintf(conn, text+string(delimiter))
+		if err != nil {
+			panic(err)
+		}
+
+		message, err := bufio.NewReader(conn).ReadString(delimiter)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Print("Response: " + message)
+	}
+}
