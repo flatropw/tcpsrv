ea29412a4abc8f57f4c84a2428a5d59a9a57ca60
root <changelater@mail.com>
2019-12-05 02:40:46 +0300

    add exit phrase to config

--- config.go
+++ config.go
@@ -1,13 +1,15 @@
 package main
 
 type configList struct {
-	address string
-	port string
-	delimiter byte
+	address    string
+	port       string
+	delimiter  byte
+	exitPhrase string
 }
 
 var config = configList{
-	address: "127.0.0.1",
-	port: "8081",
-	delimiter: '\n',
+	address:    "127.0.0.1",
+	port:       "8081",
+	exitPhrase: "exit",
+	delimiter:  '\n',
 }
