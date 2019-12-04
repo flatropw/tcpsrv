0522f6dd31611fbf84c6bc90c2ffecd8c8ecd70e
root <changelater@mail.com>
2019-12-04 23:00:16 +0300

    add simple tcp server

--- server.go
+++ server.go
@@ -1,1 +1,34 @@
-package tcpsrv
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+)
+
+func main() {
+	fmt.Println("Launching server...")
+	listener, err := net.Listen("tcp", address+":"+port)
+	if err != nil {
+		panic(err)
+	}
+
+	connection, err := listener.Accept()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Server is listening on port %s", port)
+
+	for {
+		message, err := bufio.NewReader(connection).ReadString(delimiter)
+		if err != nil {
+			panic(err)
+		}
+
+		_, err = connection.Write([]byte(message + string(delimiter)))
+		if err != nil {
+			panic(err)
+		}
+	}
+}
