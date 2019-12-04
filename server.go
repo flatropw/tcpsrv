0ee71a2b45e568a6a5d1ef3e6bb582094746f97f
root <changelater@mail.com>
2019-12-05 02:42:21 +0300

    add MultiplyIfInt function
    multiply by 2 if input string looks like int

--- server.go
+++ server.go
@@ -3,7 +3,10 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"net"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -13,23 +16,55 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Printf("Server is listening on port %s", config.port)
+	fmt.Printf("Server is listening on port %s\n", config.port)
 
 	connection, err := listener.Accept()
 	if err != nil {
-		panic(err)
+		fmt.Println(err)
 	}
-
+	defer connection.Close()
 
 	for {
 		message, err := bufio.NewReader(connection).ReadString(config.delimiter)
 		if err != nil {
-			panic(err)
+			if err != io.EOF {
+				fmt.Println(err)
+			}
+			break
+
 		}
 
-		_, err = connection.Write([]byte(message + "\n"))
+		var response string
+		message = strings.TrimSuffix(message, string(config.delimiter))
+
+		if len(message) > 0 {
+			response, err = MultiplyIfInt(message)
+			if err != nil {
+				response = strings.ToUpper(message)
+			}
+		} else {
+			response = "Message can not be empty."
+		}
+
+		_, err = connection.Write([]byte(response + string(config.delimiter)))
 		if err != nil {
-			panic(err)
+			fmt.Println(err)
+			break
 		}
 	}
+}
+
+func MultiplyIfInt(input string) (string, error) {
+	parsedFloat, err := strconv.ParseFloat(input, 10)
+	if err != nil {
+		return "", err
+	}
+
+	multiplier := 1.0
+	isInt := parsedFloat == float64(int(parsedFloat))
+	if isInt {
+		multiplier = 2.0
+	}
+
+	return fmt.Sprintf("%v", parsedFloat*multiplier), nil
 }
